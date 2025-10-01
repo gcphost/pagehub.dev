d59cb0d954c6e5f4b18a901bef35104954975a68
Will Bowman <gcphost@gmail.com>
2025-10-01 07:40:13 -0700

    braing tailwind back

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,7 @@ export default class MyDocument extends Document {
             `}
           </Script>
 
+          <link rel="stylesheet" href="/tailwind-cdn.css" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
         </Head>
