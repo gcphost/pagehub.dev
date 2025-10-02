2259f3d1bbed5f7e4ff10db0d5ed6fd4e69cc8da
Will Bowman <gcphost@gmail.com>
2025-10-02 11:23:25 -0700

    3xl empty state icon

--- components/selectors/EmptyState.tsx
+++ components/selectors/EmptyState.tsx
@@ -87,7 +87,7 @@ export const EmptyState = ({ icon = null }) => {
       ) : null}
 
       {!isActive && !isHover && icon && (
-        <div data-empty-state={true}>{icon}</div>
+        <div data-empty-state={true} className="text-3xl">{icon}</div>
       )}
     </div>
   );
