c24132be7d79f054e8851d9dfc5046e3e26672b1
Will Bowman <gcphost@gmail.com>
2025-10-02 08:14:28 -0700

    test loads

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,20 @@ import { useEffect } from "react";
 import { RecoilEnv, RecoilRoot } from "recoil";
 
 import { SessionProvider } from "next-auth/react";
+import { useRouter } from "next/router";
+import Script from "next/script";
 import "react-tooltip/dist/react-tooltip.css";
 import "../styles/app.css";
 
 RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  const router = useRouter();
+
+  // Marketing pages where we want GTM to load
+  const marketingPages = ['/', '/templates', '/privacy', '/terms', '/page-builder-api'];
+  const isMarketingPage = marketingPages.includes(router.pathname);
+
   useEffect(() => {
     const setHeight = () => {
       const vh = window.innerHeight * 0.01;
@@ -24,6 +32,18 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <RecoilRoot>
+        {isMarketingPage && (
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KQC7T779');
+            `}
+          </Script>
+        )}
+
         <Component {...pageProps} />
 
         <iframe
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import fs from "fs";
 import Document, { Head, Html, Main, NextScript } from "next/document";
-import Script from "next/script";
 import path from "path";
 
 export default class MyDocument extends Document {
@@ -8,17 +7,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en-US">
         <Head>
-          <Script id="google-tag-manager" strategy="afterInteractive">
-            {`
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KQC7T779');
-            `}
-          </Script>
-
-
           <script dangerouslySetInnerHTML={{
             __html: fs.readFileSync(path.join(process.cwd(), 'tailwind-cdn.js'), 'utf8')
           }} />
@@ -26,29 +14,6 @@ export default class MyDocument extends Document {
           {/* Preconnect to Google Fonts with crossorigin */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-
-          {/* Inline critical Tailwind base styles to prevent FOUC */}
-          <style dangerouslySetInnerHTML={{
-            __html: `
-            *, ::before, ::after {
-              box-sizing: border-box;
-              border-width: 0;
-              border-style: solid;
-              border-color: #e5e7eb;
-            }
-            html { 
-              line-height: 1.5; 
-              -webkit-text-size-adjust: 100%; 
-              -moz-tab-size: 4; 
-              tab-size: 4; 
-              font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif;
-            }
-            body { margin: 0; line-height: inherit; }
-            img, video { max-width: 100%; height: auto; display: block; }
-            h1, h2, h3, h4, h5, h6 { font-size: inherit; font-weight: inherit; }
-            a { color: inherit; text-decoration: inherit; }
-            button, input, optgroup, select, textarea { font-family: inherit; font-size: 100%; line-height: inherit; margin: 0; padding: 0; }
-          `}} />
         </Head>
         <body>
           <Main />
