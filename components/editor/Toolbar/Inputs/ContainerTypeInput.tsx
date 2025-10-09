b5f20143511fcd648905f9dcc5001d7761b53c5c
Will Bowman <gcphost@gmail.com>
2025-10-09 15:19:36 -0700

    stelle rmedia

--- components/editor/Toolbar/Inputs/ContainerTypeInput.tsx
+++ components/editor/Toolbar/Inputs/ContainerTypeInput.tsx
@@ -1,4 +1,4 @@
-import { useEditor } from "@craftjs/core";
+import { Element, useEditor } from "@craftjs/core";
 import { AddElement } from "components/editor/Viewport/Toolbox/lib";
 import { Container } from "components/selectors/Container";
 import { ToolbarItem } from "../ToolbarItem";
@@ -38,37 +38,34 @@ export const ContainerTypeInput = () => {
 
       </ToolbarItem>
 
-      {["flex-row", "flex-row-reverse"].includes(flexDirection) && (
-        <div>
-          <button
-            className="h-fill w-full btn btn-text"
-            onClick={() => {
-              AddElement({
-                element: (
-                  <Container
-                    mobile={{
-                      display: "flex",
-                      justifyContent: "justify-center",
-                      flexDirection: "flex-col",
-                      width: "w-full",
-                      gap: "gap-8",
-                      height: "h-auto",
-                      py: "py-6",
-                      px: "px-3",
-                    }}
-                    desktop={{}}
-                    custom={{ displayName: "Column" }}
-                  />
-                ),
-                actions,
-                query,
-              });
-            }}
-          >
-            Add Column
-          </button>
-        </div>
-      )}
+
+      <button
+        className="h-fill w-full btn btn-text"
+        onClick={() => {
+          AddElement({
+            element: (
+              <Element
+                canvas
+                is={Container}
+                canDelete={true}
+                mobile={{
+                  display: "flex",
+                  flexDirection: "flex-col",
+                  width: "w-full",
+                  gap: "gap-[var(--ph-section-gap)]",
+                }}
+                desktop={{}}
+                custom={{ displayName: "Container" }}
+              />
+            ),
+            actions,
+            query,
+          });
+        }}
+      >
+        Add Container
+      </button>
+
     </>
   );
 };
